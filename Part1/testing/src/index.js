aee05a30ece77faa24075fa8311eedf5b9229f1c
jackpalaia <jack@jackpalaia.com>
2020-04-23 14:58:07 -0400

    part 1.3

--- Part1/testing/src/index.js
+++ Part1/testing/src/index.js
@@ -1,24 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}. You are {props.age} years old.</p>
-    </div>
-  )
+const sum = (num1, num2) => {
+  console.log(num1);
+  console.log(num2);
+  return num1 + num2;
 }
-
-const App = () => {
-  const name = 'Jack';
-  const age = 19;
-
-  return (
-    <div>
-      <h1>Greetings!</h1>
-      <Hello name={name} age={age}/>
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(sum(1,2))
