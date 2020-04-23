ef96b24fbb77c86937f4fa0353424ad9b36c225f
jackpalaia <jack@jackpalaia.com>
2020-04-23 14:14:07 -0400

    part 1.1

--- Part1/testing/src/index.js
+++ Part1/testing/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}. You are {props.age} years old.</p>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const name = 'Jack';
+  const age = 19;
+
+  return (
+    <div>
+      <h1>Greetings!</h1>
+      <Hello name={name} age={age}/>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
