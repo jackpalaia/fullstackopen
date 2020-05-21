977215c4d49c0dd5775144191fa2eba43fe11cb4
jackpalaia <jack@jackpalaia.com>
2020-05-21 13:20:03 -0400

    exercise 3.7

--- Part3/testing/index.js
+++ Part3/testing/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
-
 const app = express()
+
 app.use(express.json())
+
+const requestLogger = (req, res, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
   {
@@ -25,42 +35,33 @@ let notes = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>hello world</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
   const note = notes.find(note => note.id === id)
-
   if (note) {
-    response.json(note)
+    res.json(note)
   } else {
-    response.status(404).end()
+    res.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
   notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  res.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(note => note.id))
-    : 0
-  return maxId + 1
-}
+app.post('/api/notes', (req, res) => {
+  const body = req.body
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  
-  if (!body.conent) {
+  if (!body.content) {
     return response.status(400).json({
       error: 'content missing'
     })
@@ -70,15 +71,25 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
   }
+
+  const id = notes.length > 0 
+    ? Math.max(...notes.map(note => note.id)) + 1
+    : 0
+  
+  note.id = id
 
   notes = notes.concat(note)
 
-  response.json(note)
+  res.json(note)
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const unknownEndpoint = (req, res) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const port = 3001
+app.listen(port)
+console.log(`server is running on port ${port}`)
