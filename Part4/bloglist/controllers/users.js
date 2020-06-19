70acdedf811f063b2e1df631f25451eda7e79d73
jackpalaia <jack@jackpalaia.com>
2020-06-19 16:20:40 -0400

    exercise 4.16

--- Part4/bloglist/controllers/users.js
+++ Part4/bloglist/controllers/users.js
@@ -2,25 +2,30 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (req, res) => {
-  const body = req.body
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash
+usersRouter.route('/')
+  .get(async (req, res) => {
+    const users = await User.find({})
+    res.send(users.map(u => u.toJSON()))
   })
-
-  const savedUser = await user.save()
-
-  res.send(savedUser.toJSON())
-})
-
-usersRouter.get('/', async (req, res) => {
-  const users = await User.find({})
-  res.send(users.map(u => u.toJSON()))
-})
+  .post(async (req, res) => {
+    const body = req.body
+  
+    if (!body.password || body.password.length < 3) {
+      res.status(400).json({ error: 'password missing or password under 3 characters' })
+    }
+  
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash
+    })
+  
+    const savedUser = await user.save()
+  
+    res.send(savedUser.toJSON())
+  })
 
 module.exports = usersRouter
