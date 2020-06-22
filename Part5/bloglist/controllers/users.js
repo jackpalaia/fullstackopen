8d4aa79227ab894b4f7d0947295cdb4a1098aed4
jackpalaia <jack@jackpalaia.com>
2020-06-22 15:12:57 -0400

    initial commit

---
+++ Part5/bloglist/controllers/users.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.route('/')
+  .get(async (req, res) => {
+    const users = await User.find({}).populate('blogs', { user: 0 })
+    res.send(users.map(u => u.toJSON()))
+  })
+  .post(async (req, res) => {
+    const body = req.body
+  
+    if (!body.password || body.password.length < 3) {
+      res.status(400).json({ error: 'password missing or password under 3 characters' })
+    }
+  
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash
+    })
+  
+    const savedUser = await user.save()
+  
+    res.send(savedUser.toJSON())
+  })
+
+module.exports = usersRouter
---
+++ Part5/notes/build/precache-manifest.94296bbe7e1a007b1a748b5e2f186dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020cb12716acda15bcd5979dce154ab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0e1184d997507af0fd",
+    "url": "/static/css/main.3ae13fbb.chunk.css"
+  },
+  {
+    "revision": "be718a7d55be9239779a",
+    "url": "/static/js/2.5ab3afcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ab3afcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0e1184d997507af0fd",
+    "url": "/static/js/main.93a4f80d.chunk.js"
+  },
+  {
+    "revision": "9f10ad7796608ffb84b7",
+    "url": "/static/js/runtime-main.1b50fb31.js"
+  }
+]);
