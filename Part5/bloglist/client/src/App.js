655aaa9f85f8578ea4bbec7e549faf52a1fd38bd
jackpalaia <jack@jackpalaia.com>
2020-06-22 16:20:42 -0400

    exercise 5.1

--- Part5/bloglist/client/src/App.js
+++ Part5/bloglist/client/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +16,27 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (e) {
+      console.log('wrong credentials')
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+        <Login login={handleLogin} u={username} uChange={({ target }) => setUsername(target.value)} p={password} pChange={({ target }) => setPassword(target.value)} /> :
+        <div>
+          <p>{user.name} logged in</p>
+          <Blogs blogs={blogs} />
+        </div>
+      }
     </div>
   )
 }
